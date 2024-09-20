6e31fd18fa4a149ab4587628cdb4d92a9261b7d3
Joao-Lucas-de-Freitas-Rodrigues <joaolucasfreitas360@gmail.com>
2024-09-20 01:43:22 -0300

    Removendo comentarios

--- Backend/ContainerManagement/Services/ContainerColorDetection.cs
+++ Backend/ContainerManagement/Services/ContainerColorDetection.cs
@@ -10,17 +10,13 @@ namespace ContainerManagement.Services
     {
         public string DetectContainerColor(byte[] imageBytes)
         {
-            // Salvar a imagem em um arquivo temporário
             string tempFilePath = Path.GetTempFileName() + ".jpg";
             File.WriteAllBytes(tempFilePath, imageBytes);
 
-            // Carregar a imagem do arquivo temporário
             var image = new Image<Bgr, byte>(tempFilePath);
 
-            // Converter a imagem para o espaço de cores HSV
             var hsvImage = image.Convert<Hsv, byte>();
 
-            // Definir as faixas de cores HSV para detecção de cores (Exemplo: Azul, Vermelho, Verde)
             var lowerBlue = new Hsv(100, 150, 50);
             var upperBlue = new Hsv(130, 255, 255);
 
@@ -30,19 +26,15 @@ namespace ContainerManagement.Services
             var lowerGreen = new Hsv(35, 100, 100);
             var upperGreen = new Hsv(85, 255, 255);
 
-            // Detectar a cor Azul
             var blueMask = hsvImage.InRange(lowerBlue, upperBlue);
-            var blueCount = blueMask.CountNonzero()[0];  // Contar os pixels azuis
+            var blueCount = blueMask.CountNonzero()[0];
 
-            // Detectar a cor Vermelha
             var redMask = hsvImage.InRange(lowerRed, upperRed);
-            var redCount = redMask.CountNonzero()[0];  // Contar os pixels vermelhos
+            var redCount = redMask.CountNonzero()[0];
 
-            // Detectar a cor Verde
             var greenMask = hsvImage.InRange(lowerGreen, upperGreen);
-            var greenCount = greenMask.CountNonzero()[0];  // Contar os pixels verdes
+            var greenCount = greenMask.CountNonzero()[0];
 
-            // Determinar a cor predominante
             string detectedColor;
             if (blueCount > redCount && blueCount > greenCount)
             {
@@ -61,7 +53,6 @@ namespace ContainerManagement.Services
                 detectedColor = "Cor não detectada";
             }
 
-            // Excluir o arquivo temporário
             File.Delete(tempFilePath);
 
             return detectedColor;
